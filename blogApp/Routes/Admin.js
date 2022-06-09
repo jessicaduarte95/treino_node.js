38e4b3da3c5010cdea5a9bba048629111bdc0ae3
jessicaduarte95 <jessicaduarte.95@gmail.com>
2022-06-09 18:21:02 -0300

    Atualização

--- blogApp/Routes/Admin.js
+++ blogApp/Routes/Admin.js
@@ -64,9 +64,6 @@ router.get("/categorias/edit/:id", (req,res) => {
         req.flash("error_msg", "Esta categoria não existe.")
         res.redirect("/admin/categorias")
     })
-    // Categoria.findOne({_id:req.params.id}).lean().then((categoria)=>{
-    //     res.render('admin/editcategorias', {categoria:categoria})
-    // })
 })
 
 router.post("/categorias/edit", (req, res) => {
@@ -76,21 +73,39 @@ router.post("/categorias/edit", (req, res) => {
         categoria.slug = req.body.slug
 
         categoria.save().then(() => {
-            res.flash()("success", "Categoria editada com sucesso.")
-            res.redirect("/admin/categorias")
+            req.flash('success', 'Categoria editada com sucesso.')
+            res.redirect('/admin/categorias')
         }).catch((err) => {
-            req.flash("error_msg", "Houve um erro interno ao salvar a edição da categoria.")
-            res.redirect("/admin/categorias")
+            req.flash('error_msg', 'Houve um erro interno ao salvar a edição da categoria.')
+            res.redirect('/admin/categorias')
         })
 
     }).catch((err) => {
-        req.flash("error_msg", "Houve um erro ao editar a categoria.")
+        req.flash('error_msg', 'Houve um erro ao editar a categoria.')
+        res.redirect('/admin/categorias')
+    })
+})
+
+router.post("/categorias/deletar", (req,res) => {
+    Categoria.deleteOne({_id: req.body.id}).then(() => {
+        req.flash("success_msg", "Categoria deletada com sucesso!")
+        res.redirect("/admin/categorias")
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao deletar categoria!")
         res.redirect("/admin/categorias")
     })
 })
 
 router.get('/categorias/add', (req,res) => {
     res.render('admin/addcategoria')
+})
+
+router.get('/postagens', (req,res) => {
+    res.render("admin/postagens")
+})
+
+router.get('/postagens/add', (req,res) => {
+    res.render("admin/addPostagem")
 })
 
 module.exports = router // É necessário exportar no final do arquivo.
